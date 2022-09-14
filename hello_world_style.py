da718dd5a9e62cb0866e9e0fc92ab313b24da1bf
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-14 18:31:23 +0000

    Created Hello, World program with style

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Marcus Wehbi
+# Date: Sep. 14, 2022
+# This program displays Hello, World! with proper style.
+
+def main():
+    print("Hello, World! with proper style!")
+    print("Completed in Python!")
+
+if __name__ == "__main__":
+    main()
